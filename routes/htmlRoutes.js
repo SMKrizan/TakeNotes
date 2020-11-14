39bb7a589876a55851fa0cd3affd1c653b1e21ae
SMKrizan <smkrizan@gmail.com>
2020-11-13 23:41:18 -0600

    files reverted ready to submit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,13 +8,13 @@ const path = require('path');
 
 // GET route to return notes.html file.
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 // GET route to handle index.html
 router.get('*', (req, res) => {
     // responds with html file to display in browser; 'path' module ensures that correct html location is found regardless of server environment
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
   module.exports = router;
